86f46f3ed351eca77128a0c00ad1b37c85cfbee2
Fakhrul-Hakimy <165546343+Fakhrul-Hakimy@users.noreply.github.com>
2025-11-16 01:53:32 +0800

    Initialize React + Vite project structure
    Add initial project files for a React app using Vite, including configuration, source files, assets, and ESLint setup.

---
+++ ANIMEDECK/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import menu from './menu.jsx';
+
+const App = () => {
+  return (
+    <div className="header">
+      
+      <menu />
+    </div>
+
+    
+  );
+}
+export default App;
