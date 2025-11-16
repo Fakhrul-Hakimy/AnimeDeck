ed662e090d54b17ae17612a24242833236d471b6
Fakhrul-Hakimy <165546343+Fakhrul-Hakimy@users.noreply.github.com>
2025-11-16 17:57:41 +0800

    Fetch and display anime details from API
    Added API call to fetch full anime details using Jikan API based on the URL id. Updated component to display anime name and mal_id from fetched data, and included useEffect hooks for data fetching and logging.

--- ANIMEDECK/src/details.jsx
+++ ANIMEDECK/src/details.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
+
+const BASE_URL = "https://api.jikan.moe/v4"
+const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        Accept: "application/json"
+    }
+}
 
 const Details = () => {
     const { id } = useParams();
+    const [animeDetails, setAnimeDetails] = useState(null);
+
+
+    const getDetails = async () => {
+
+        try{
+            const endpoint=`${BASE_URL}/anime/${id}/full`;
+            const res = await fetch(endpoint);
+            const data = await res.json();
+            setAnimeDetails(data.data);
+
+
+        }catch(error){
+            console.log(error);
+        }
+
+    }
+    useEffect(() => {
+        getDetails();
+    },[id])
+
+    useEffect(() => {
+        console.log("Updated anime details:", animeDetails);
+    }, [animeDetails]);
 
     return (
         <div className="text-white">
             <h1>Anime Details</h1>
-            <p>ID from URL: {id}</p>
+            <p>ID from URL: {animeDetails?.mal_id}</p>
+            <p>Anime Name : {animeDetails?.title}</p>
         </div>
     );
 };
