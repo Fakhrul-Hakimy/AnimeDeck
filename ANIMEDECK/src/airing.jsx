9f2972224059a2d33c496a42b657f628af1da3e2
Fakhrul-Hakimy <165546343+Fakhrul-Hakimy@users.noreply.github.com>
2025-11-16 15:12:58 +0800

    Add pagination to Airing anime list
    Introduced pagination controls to the Airing component, allowing users to navigate between pages of anime schedules. Updated state management and API calls to support page selection, and added corresponding styles for the pagination UI in index.css.

--- ANIMEDECK/src/airing.jsx
+++ ANIMEDECK/src/airing.jsx
@@ -15,18 +15,23 @@ const Airing =  () => {
 
     const [airingList, setAiringList] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [pageNumber, setPageNumber] = useState(1)
+    const [maxPage, setMaxPage] = useState(0)
+    const [page, setPage] = useState(0)
 
 
     const getAiring = async () => {
         setIsLoading(true);
             try{
 
-                const endpoint = `${BASE_URL}/schedules`;
+                const endpoint = `${BASE_URL}/schedules?page=${pageNumber}`;
                 const result = await fetch(endpoint,API_OPTIONS );
 
                 const data = await result.json();   // <-- FIX HERE
                 setAiringList(data.data);
-                console.log(data.data);
+                setMaxPage(data.pagination.last_visible_page);
+                setPage(data.pagination.current_page);
+
 
             }catch(error){
                 console.log(error);
@@ -38,7 +43,7 @@ const Airing =  () => {
 
     useEffect(() => {
         getAiring();
-    },[]);
+    },[pageNumber]);
 
 
 
@@ -55,8 +60,39 @@ const Airing =  () => {
                     <Card key={anime.mal_id} anime={anime} />
                 ))}
             </div>
+
+                <div className="pagination flex items-center gap-2">
+
+                    <button
+                        className="px-4 py-2 bg-gray-700 text-white rounded-l-lg border-2"
+                        onClick={() => pageNumber > 1 && setPageNumber(pageNumber - 1)}
+                    >
+                        Prev
+                    </button>
+
+                    <input
+                        type="number"
+                        value={page}
+                        onChange={(e) => setPageNumber(Number(e.target.value))}
+                        min={1}
+                        max={maxPage}
+                        className="w-12 px-1 py-1 text-center bg-gray-900 text-white border-2 border-gray-400 rounded focus:border-blue-400 focus:outline-none"
+                    />
+
+
+                    <span className="text-white">of {maxPage}</span>
+
+                    <button
+                        className="px-4 py-2 bg-gray-700 text-white rounded-r-lg"
+                        onClick={() => pageNumber < maxPage && setPageNumber(pageNumber + 1)}
+                    >
+                        Next
+                    </button>
+
+                </div>
             </div>
-}
+
+           }
        </div>
     )
 }
