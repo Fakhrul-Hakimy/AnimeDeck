492968186b6e2671914ee95d0c79436dfdc66d90
Fakhrul-Hakimy <165546343+Fakhrul-Hakimy@users.noreply.github.com>
2025-12-06 22:59:56 +0800

    Add API call for anime recommendations in Sidebar
    Introduced a fetch request to the Jikan API for anime recommendations within the Sidebar component. The fetched data is currently logged to the console.

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Sidebar = () => {
+    const APIurl="https://api.jikan.moe/v4/recommendations/anime";
+
+    function getRecommendations() {
+        fetch(APIurl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+
+    getRecommendations();
     return (
         <div className="sidebar">
             <span className="badge text-bg-secondary">Recommendation</span>
+
         </div>
     )
 }
