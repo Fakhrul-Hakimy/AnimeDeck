fab0b2d05a6a71c35ad710a2ab3a2487fa1ea096
Fakhrul-Hakimy <165546343+Fakhrul-Hakimy@users.noreply.github.com>
2025-12-07 17:23:33 +0800

    Enhance sidebar with recommendations and styling
    Sidebar now fetches and displays anime recommendations from the Jikan API using useEffect and useState. Added new CSS classes for improved card styling, text truncation, and scrollbar handling to enhance UI appearance and usability.

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Sidebar = () => {
     const APIurl="https://api.jikan.moe/v4/recommendations/anime";
+    const [recommend, setRecommend] = useState( []);
 
     function getRecommendations() {
         fetch(APIurl)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            setRecommend(data.data);
         })
     }
 
-    getRecommendations();
+    useEffect(() => {
+        getRecommendations();
+    },[])
+
+
+
     return (
-        <div className="sidebar">
-            <span className="badge text-bg-secondary">Recommendation</span>
+        <div className="sidebar flex-column">
+
+            <span className="badge text-bg-secondary mb-2 d-block text-center truncate-badge">Recommendation</span>
+            {recommend.map(item => (
+            <div className="card mb-2 custom-dark-card" key={item.id}>
+                <h5 className="card-header d-block text-center " onClick={() => window.location.href = item.user.url}>{item.user.username}</h5>
+                <div className="card-body">
+                    {item.entry.map(entry => (
+                        <h5 className="badge d-block text-center text-bg-secondary truncate-badge m-1">{entry.title}</h5>
+                    ))}
+
+                    <p className="card-text clamp-3">{item.content}</p>
+                </div>
+            </div>
+
+
+
+            ))}
 
         </div>
     )
